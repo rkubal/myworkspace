ade7df90c0f0db5def9cb1480cb8884be14853fc
swapn <swapn@LAPTOP-9P1CPNU0.Home>
2020-03-08 15:39:25 +0000

    added a method which returns a map object

--- course-api-data-1/src/main/java/com/training/springbootstarter/controller/HelloController.java
+++ course-api-data-1/src/main/java/com/training/springbootstarter/controller/HelloController.java
@@ -1,14 +1,28 @@
 package com.training.springbootstarter.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+	
+	Map<String, Integer> resultMap = new HashMap<String, Integer>();
 
 	@RequestMapping("/hello")//get methods mapping, which is by default so we don't need to 
 	//explicitly mention it
 	public String greet() {
 		return "hello welcome";
+	}
+	
+	@RequestMapping("/getMap")
+	public Map<String, Integer> getResultMap() {
+		resultMap.put("banana", 1);
+		resultMap.put("apple", 2);
+		resultMap.put("strawberry", 4);
+		resultMap.put("watermelon", 2);
+		return resultMap;
 	}
 }
